ffaea24ab71392728fc56b0d218dbbbce8c80789
tiisetsopudinyane <pudinyanesamueltiisetso@gmail.com>
2023-05-30 11:15:19 +0200

    added the last js pages of transportFee function

---
+++ test/transportFee.tests.js
@@ -0,0 +1,5 @@
+describe("this test transport fee function", function(){
+    it("should return R20",function(){
+        assert.equal(transportFee("morning"),"R20","must return R20 if its Morning")
+    });
+});
---
+++ transportFee.js
@@ -0,0 +1,8 @@
+function transportFee(dayTime){
+switch(dayTime){
+  case "morning": return "R20";
+    case "afternoon": return "R10";
+    default: return "free";
+    
+              }
+}
